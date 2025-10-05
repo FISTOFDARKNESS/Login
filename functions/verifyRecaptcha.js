a7b73959e0d90a321262134cebd41e084ae18c52
_.yzero <116497775+FISTOFDARKNESS@users.noreply.github.com>
2025-10-05 14:59:46 +0200

    Update verifyRecaptcha.js

--- functions/verifyRecaptcha.js
+++ functions/verifyRecaptcha.js
@@ -1,14 +1,13 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
-  // Permitir CORS
+
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Content-Type',
     'Access-Control-Allow-Methods': 'POST, OPTIONS'
   };
 
-  // Handle preflight OPTIONS request
   if (event.httpMethod === 'OPTIONS') {
     return {
       statusCode: 200,
@@ -37,7 +36,7 @@ exports.handler = async (event) => {
     }
 
     // Verificar o token com o Google reCAPTCHA v3
-    const secretKey = '6LdOEdgrAAAAANKOW3tk8LYlR4x2aL0KXtb0fwj-';
+    const secretKey = '6LdvNN8rAAAAACsHw91wyveLBTQYnFqeqkHsOFTZ';
     const verificationUrl = `https://www.google.com/recaptcha/api/siteverify`;
     
     const response = await fetch(verificationUrl, {
